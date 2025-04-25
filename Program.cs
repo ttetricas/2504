850e7a0dc0a7b8f5ab5ce3c4297828fe62aac7ca
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 19:52:06 -0300

    aula decisoesõõ

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Aula 25.04 -Decisões-
+
+string senhaDigitada;
+string senhaEsperada = "1224";
+
+Console.Write("Digite sua senha: ");
+senhaDigitada = Console.ReadLine()!;
+
+if (senhaDigitada == senhaEsperada)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("SUCESSO");
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("ERRO, TENTE NOVAMENTE");
+}
+Console.ResetColor();
